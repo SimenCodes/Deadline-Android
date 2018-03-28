0f213f4054442b108f442d302e15152b161c6834
Simen Burud <sb@simen.codes>
2018-03-28 17:28:33 +0200

    Interactivity (Kotlin)
    https://simen.codes/hs-android/interactivity-kotlin/

--- app/src/main/java/no/hackerspacentnu/deadline/MainActivity.kt
+++ app/src/main/java/no/hackerspacentnu/deadline/MainActivity.kt
@@ -2,11 +2,24 @@ package no.hackerspacentnu.deadline
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // hoursLeftTextView.text = "X hours left" // Now we update the text!
+    }
+
+    /*
+     * This code is run when the “Change deadline” button is clicked.
+     * We set the onClick listener attribute of the button in `activity_main.xml` to make it happen.
+     */
+    fun onChangeDeadlineClicked(viewThatWasClicked: View) {
+        val hoursLeft = Math.round(Math.random() * 100)
+        hoursLeftTextView.text = "$hoursLeft hours left"
     }
 }
