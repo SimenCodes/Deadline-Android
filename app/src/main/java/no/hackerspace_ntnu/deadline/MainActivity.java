579f8a03b6d7bbc972b481d343b8b9add3c046a2
Simen Burud <sb@simen.codes>
2018-03-28 17:01:53 +0200

    Interactivity
    https://simen.codes/hs-android/interactivity/

--- app/src/main/java/no/hackerspace_ntnu/deadline/MainActivity.java
+++ app/src/main/java/no/hackerspace_ntnu/deadline/MainActivity.java
@@ -1,13 +1,30 @@
 package no.hackerspace_ntnu.deadline;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hoursLeftTextView; // Lets us “do stuff” with the text view.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Look up the text view using the ID we set in `activity_main.xml`
+        hoursLeftTextView = findViewById(R.id.hoursLeftTextView);
+        // hoursLeftTextView.setText("X hours left"); // Now we update the text!
+    }
+
+    /*
+     * This code is run when the “Change deadline” button is clicked.
+     * We set the onClick listener attribute of the button in `activity_main.xml` to make it happen.
+     */
+    public void onChangeDeadlineClicked(View viewThatWasClicked) {
+        int hoursLeft = (int) (Math.random() * 100);
+        hoursLeftTextView.setText(hoursLeft + " hours left");
     }
 }
